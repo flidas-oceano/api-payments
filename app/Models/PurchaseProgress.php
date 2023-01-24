234bfda7d93f77a67224691994479964a67015da
Gomez Tomas Gonzalo <tomasgomez@oceano.com.ar>
2023-01-24 09:04:57 -0300

    Al tomar el modelo agrega tablas foreanas

--- app/Models/PurchaseProgress.php
+++ app/Models/PurchaseProgress.php
@@ -11,17 +11,19 @@ class PurchaseProgress extends Model
     use HasFactory;
 
     protected $table = 'purchase_progress';
-    //protected $fillable = ['lead_id_fk'];
     protected $hidden = ['created_at','updated_at'];
 
     protected $guarded = [];
     public function leads(){
-        $leads = $this->hasOne(Lead::class,'id','lead_id_fk');
+        $leads = $this->hasOne(Lead::class,'id','lead_id');
         return $leads;
     }
 
     public static function getModel($purchaseProgressId){
-        return PurchaseProgress::where('id', $purchaseProgressId)->first();
+        $progress = PurchaseProgress::where('id', $purchaseProgressId)->first();
+        $progress->lead = $progress->hasOne(Lead::class,'id','lead_id')->first();
+        
+        return $progress;
     }
 
     public static function updateProgress($purchaseProgressId, $values){
