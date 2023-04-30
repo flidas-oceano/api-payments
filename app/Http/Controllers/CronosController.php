5261c9134f505cf54b21add44f0cacbaed69a7d5
mariano <marianohayon@oceano.com.ar>
2023-04-30 15:52:28 -0300

    # x

--- app/Http/Controllers/CronosController.php
+++ app/Http/Controllers/CronosController.php
@@ -317,7 +317,7 @@ class CronosController extends Controller
                     //==========arranca empaquetado de datos
 
                     $pack = $this->packData($crude, $e->type);
-                    $pack['prueba'] = 1;
+                    //$pack['prueba'] = 1;
 
                     //====== termina empaquetado de datos
 
@@ -351,7 +351,7 @@ class CronosController extends Controller
             }
 
         //caso especial
-        $special = true;
+        $special = false;
         $packs[$e->id] = $pack;
 
             if (!$ignore) 
