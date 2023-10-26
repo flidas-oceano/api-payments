e9179443d7fb51a3df2c5573988783b7f3915692
fbrizuelaoceano <facundobrizuela@oceano.com.ar>
2023-10-26 16:49:58 -0300

    Nueva tabla flow_spp

---
+++ database/migrations/2023_10_26_160705_add_foreign_key_to_placetopay_transactions.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeyToPlacetopayTransactions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('placetopay_transactions', function (Blueprint $table) {
+            $table->foreignId('flow_spp')
+                ->nullable()
+                ->default(null)
+                ->references('id')
+                ->on('flows_spp');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('placetopay_transactions', function (Blueprint $table) {
+            $table->dropForeign(['flow_spp']);
+            $table->dropColumn('flow_spp');
+        });
+    }
+}
